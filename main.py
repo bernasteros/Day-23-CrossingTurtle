d8d2379ab6e4ac899971e27a5e141217b5ee4e72
Bernhard Stern <bernhard.stern@keemail.me>
2021-11-10 17:53:58 +0100

    Write Player class Initialize turtle Set BGColor to black

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+HEIGHT = 600
+WIDTH = 600
+
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(WIDTH, HEIGHT)
 screen.tracer(0)
+screen.bgcolor("black")
+screen.title("Turtle Crossing")
+
+froggy = Player()
+
 
 game_is_on = True
 while game_is_on:
+
     time.sleep(0.1)
     screen.update()
--- player.py
+++ player.py
@@ -1,7 +1,22 @@
-STARTING_POSITION = (0, -280)
+from turtle import Turtle
+
+HEIGHT = 600
+STARTING_POSITION = (0, (HEIGHT-20)*-1)
 MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
+FINISH_LINE_Y = HEIGHT-20
+COLOR = "white"
+FACE_UP = 90
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.color(COLOR)
+        self.setpos(STARTING_POSITION)
+        self.showturtle()
+        self.setheading(FACE_UP)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
