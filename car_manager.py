0ead7df39005a1b5f8c698681589ace67ddd8443
Bernhard Stern <bernhard.stern@keemail.me>
2021-11-13 19:14:59 +0100

    Create Car Constructor (CarManager)

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+from random import choice, randint
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        new_car = Turtle()
+        new_car.shapesize(stretch_wid=2, stretch_len=1)
+        new_car.penup()
+        new_car.color(choice(COLORS))
+        random_y = randint(-250, 250)
+        new_car.goto(300, random_y)
+        self.all_cars.append(new_car)
