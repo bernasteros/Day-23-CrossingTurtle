bb5dc2147ccdefe3b0a826c9f1329ba72fe4de62
Bernhard Stern <bernhard.stern@keemail.me>
2021-11-14 16:31:04 +0100

    Take Scoreboard Class from former game

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,25 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+STYLE = ('Courier', 30, 'bold')
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.sety(250)
+        self.color("white")
+        self.penup()
+        self.count = 0
+        self.write("Score:" + str(self.count), font=STYLE, align='center')
+
+    def count_up(self):
+        self.count += 1
+        self.clear()
+        self.write("Score:" + str(self.count), font=STYLE, align='center')
+
+    def game_over(self):
+        self.clear()
+        self.color("red")
+        self.sety(0)
+        self.write("Game Over!\n Final Score:" + str(self.count) + "crossings", font=STYLE, align='center')
